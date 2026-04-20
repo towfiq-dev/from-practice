a5c7198c7a3c16db5f88b34fa2da213547cf3fce
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-04-20 20:24:30 +0600

    Auto-update on Mon, Apr 20, 2026  8:24:30 PM

--- src/components/useRef/UseRef.jsx
+++ src/components/useRef/UseRef.jsx
@@ -1,26 +1,39 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 const UseRef = () => {
-  const nameRef = useRef()
+  const nameRef = useRef(null)
+  const emailRef = useRef('')
+  const passRef = useRef('')
+  const [error, setError] = useState('')
   const Ref=(e)=>{
     e.preventDefault()
+    
+    if (passRef.current.value.length < 5) {
+      setError('sorry for that')
+    }else{
+      setError('')
+      console.log(nameRef.current.value);
+    console.log(emailRef.current.value);
+    console.log(passRef.current.value);
+    }
   }
   
 
   return (
-    <div onSubmit={Ref}>
-      <form className='grid justify-center mt-15'>
+    <div >
+      <form onSubmit={Ref}  className='grid justify-center mt-15'>
       <label className='text-[18px] font-medium' htmlFor="">Name</label>
-      <input onChange={} className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="text" name='name' id="" placeholder='Enter your name'/> <br />
+      <input ref={nameRef} className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="text" name='name' id="" placeholder='Enter your name'/> <br />
 
       <label className='text-[18px] font-medium' htmlFor="">Email</label>
-      <input  className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="email" name='email' id="" placeholder='Enter your email'/> <br />
+      <input ref={emailRef} defaultValue={'fgvc@gmail.com'}  className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="email" name='email' id="" placeholder='Enter your email'/> <br />
 
       <label className='text-[18px] font-medium' htmlFor="">Password</label>
-      <input className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="password" name='password' id="" placeholder='Enter your password'/> <br />
+      <input ref={passRef} className='border-2 border-black text-[17px] font-medium w-80 h-5 rounded py-5 px-2 ' type="password" name='password' id="" placeholder='Enter your password'/> <br />
 
       <input className='btn text-[17px] font-semibold' type="submit" value="Submit" name='' id=""/>
       </form> 
+      <p>{error}</p>
     </div>
   );
 };
